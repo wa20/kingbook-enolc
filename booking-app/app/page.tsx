e9649b337c4cc796ac287a6acbd2011f5c829273
wael <78425538+wa20@users.noreply.github.com>
2024-05-18 12:24:49 +0100

    added calander section to search form

--- booking-app/app/page.tsx
+++ booking-app/app/page.tsx
@@ -5,10 +5,10 @@ import SearchForm from "../components/SearchForm";
 export default function Home() {
   return (
     <main className="bg-[#013B94]">
-      <section className="max-w-7xl mx-auto p-7">
-        <h2 className="font-bold text-5xl text-white"> Find Your Next Stay</h2>
-        <h3 className=" text-white py-5 text-xl">
-          Search low prices on hotes, homes and much more...
+      <section className="max-w-7xl mx-auto p-6">
+        <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
+        <h3 className="text-white py-5 text-xl">
+          Search low prices on hotels, homes and much more...
         </h3>
       </section>
 
@@ -23,26 +23,22 @@ export default function Home() {
             Most popular choices for travellers from around the world
           </p>
         </div>
-        <div className="flex space-x-4 py-5 overflow-x-auto">
-            {trending_data.map(item => (
-              <div 
-              key={item.id}
-              className="space-y-1 shrink-0 cursor-pointer"
-              >
-               <img 
+
+        <div className="flex space-x-4 py-5 overflow-x-scroll">
+          {trending_data.map((item) => (
+            <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
+              <img
                 key={item.id}
                 className="w-80 h-72 object-cover rounded-lg pb-2"
                 src={item.src}
                 alt={item.title}
-               />
+              />
 
-               <p className="font-bold">{item.title}</p>
-               <p className="">{item.location}</p>
-               <p className="font-light text-sm">{item.description}</p>
-
-                
-              </div>
-            ))}
+              <p className="font-bold">{item.title}</p>
+              <p className="">{item.location}</p>
+              <p className="font-light text-sm">{item.description}</p>
+            </div>
+          ))}
         </div>
       </section>
     </main>
--- booking-app/components/SearchForm.tsx
+++ booking-app/components/SearchForm.tsx
@@ -15,28 +15,32 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { BedDoubleIcon, CalendarDaysIcon } from "lucide-react";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { BedDoubleIcon, CalendarDaysIcon, CalendarIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import { cn } from "../lib/utils";
+import { format } from "date-fns";
+import { Calendar } from "./ui/calendar";
 
 export const formSchema = z.object({
-    location: z.string().min(2, "Must be 2 characters or more").max(50),
-    dates: z.object({
-      from: z.date(),
-      to: z.date(),
-    }),
-    adults: z
-      .string()
-      .min(1, {
-        message: "Please select at least 1 adult",
-      })
-      .max(12, { message: "Max 12 adults Occupancy" }),
-    children: z.string().min(0).max(12, {
-      message: "Max 12 children Occupancy",
-    }),
-    rooms: z.string().min(1, {
-      message: "Please select at least 1 room",
-    }),
-  });
+  location: z.string().min(2, "Must be 2 characters or more").max(50),
+  dates: z.object({
+    from: z.date(),
+    to: z.date(),
+  }),
+  adults: z
+    .string()
+    .min(1, {
+      message: "Please select at least 1 adult",
+    })
+    .max(12, { message: "Max 12 adults Occupancy" }),
+  children: z.string().min(0).max(12, {
+    message: "Max 12 children Occupancy",
+  }),
+  rooms: z.string().min(1, {
+    message: "Please select at least 1 room",
+  }),
+});
 
 const SearchForm = () => {
   const router = useRouter();
@@ -59,10 +63,10 @@ const SearchForm = () => {
   return (
     <div>
       <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounded-lg"
-      >
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounded-lg"
+        >
           <div className="grid w-full lg:max-w-sm items-center gap-1.5">
             <FormField
               control={form.control}
@@ -71,46 +75,75 @@ const SearchForm = () => {
                 <FormItem>
                   <FormLabel className=" flex text-white">
                     Location
-                    <BedDoubleIcon className="ml-2 h-4 w-4 text-white"/>
+                    <BedDoubleIcon className="ml-2 h-4 w-4 text-white" />
                   </FormLabel>
 
                   <FormMessage />
 
                   <FormControl>
                     <Input placeholder="Manchester, UK" {...field} />
-                  </FormControl> 
+                  </FormControl>
                 </FormItem>
               )}
             />
           </div>
           <div className="grid w-full lg-max-w-sm felx-1 items-center gap-1.5">
-             <FormField
+            <FormField
               control={form.control}
               name="dates"
               render={({ field }) => (
                 <FormItem className="flex flex-col">
                   <FormLabel className=" flex text-white">
                     Dates
-                    <CalendarDaysIcon className="ml-2 h-4 w-4 text-white"/>
+                    <CalendarDaysIcon className="ml-2 h-4 w-4 text-white" />
                   </FormLabel>
                   <FormMessage />
 
                   <Popover>
                     <PopoverTrigger asChild>
                       <FormControl>
-
-                        
-
+                        <Button
+                          id="date"
+                          name="dates"
+                          variant={"outline"}
+                          className={cn(
+                            "w-full lg:w-[300px] justify-start text-left font-normal",
+                            !field.value.from && "text-muted-foreground"
+                          )}
+                        >
+                          <CalendarIcon className="mr-3 h-4 w-4 opacity-50" />
+                          {field.value?.from ? (
+                            field.value?.to ? (
+                              <>
+                                {format(field.value?.from, "LLL dd, y")} -{" "}
+                                {format(field.value?.to, "LLL dd, y")}
+                              </>
+                            ) : (
+                              format(field.value?.from, "LLL dd, y")
+                            )
+                          ) : (
+                            <span>Select your dates</span>
+                          )}
+                        </Button>
                       </FormControl>
                     </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        initialFocus
+                        mode="range"
+                        selected={field.value}
+                        defaultMonth={field.value.from}
+                        onSelect={field.onChange}
+                        numberOfMonths={2}
+                        disabled={(date) =>
+                          date < new Date(new Date().setHours(0, 0, 0, 0))
+                        }
+                      />
+                    </PopoverContent>
                   </Popover>
-
                 </FormItem>
-
               )}
-             >
-
-             </FormField>
+            ></FormField>
           </div>
         </form>
       </Form>
