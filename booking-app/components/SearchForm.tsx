a49523bb26ddd0dc51edbf5eab3a0c22b86c06bd
wael <78425538+wa20@users.noreply.github.com>
2024-05-18 12:19:39 +0100

    additional packages installed including calander from shadcn also added intial set up for calander section on searchForm

--- booking-app/components/SearchForm.tsx
+++ booking-app/components/SearchForm.tsx
@@ -15,27 +15,28 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { BedDoubleIcon } from "lucide-react";
+import { BedDoubleIcon, CalendarDaysIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 export const formSchema = z.object({
-  location: z.string().min(2, "Must be 2 characters or more").max(50),
-  dates: z.object({
-    from: z.date(),
-    to: z.date(),
-  }),
-  adults: z
-    .string()
-    .min(1, {
-      message: "Please select at least 1 adult",
-    })
-    .max(12, { message: "Max 12 adults Occupancy" }),
-  children: z.string().min(0).max(12, {
-    message: "Max 12 children Occupancy",
-  }),
-  rooms: z.string().min(1, {
-    message: "Please select at least 1 room",
-  }),
-});
+    location: z.string().min(2, "Must be 2 characters or more").max(50),
+    dates: z.object({
+      from: z.date(),
+      to: z.date(),
+    }),
+    adults: z
+      .string()
+      .min(1, {
+        message: "Please select at least 1 adult",
+      })
+      .max(12, { message: "Max 12 adults Occupancy" }),
+    children: z.string().min(0).max(12, {
+      message: "Max 12 children Occupancy",
+    }),
+    rooms: z.string().min(1, {
+      message: "Please select at least 1 room",
+    }),
+  });
 
 const SearchForm = () => {
   const router = useRouter();
@@ -58,11 +59,11 @@ const SearchForm = () => {
   return (
     <div>
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg-space-y-0 rounded-lg"
-        >
-          <div className="grid w-fill lg:max-w-sm items-center gap-1.5">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounded-lg"
+      >
+          <div className="grid w-full lg:max-w-sm items-center gap-1.5">
             <FormField
               control={form.control}
               name="location"
@@ -78,13 +79,38 @@ const SearchForm = () => {
                   <FormControl>
                     <Input placeholder="Manchester, UK" {...field} />
                   </FormControl> 
-                  {/* <FormDescription>
-                    This is your public display name.
-                  </FormDescription> */}
-                  {/* <FormMessage /> */}
                 </FormItem>
               )}
             />
+          </div>
+          <div className="grid w-full lg-max-w-sm felx-1 items-center gap-1.5">
+             <FormField
+              control={form.control}
+              name="dates"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className=" flex text-white">
+                    Dates
+                    <CalendarDaysIcon className="ml-2 h-4 w-4 text-white"/>
+                  </FormLabel>
+                  <FormMessage />
+
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+
+                        
+
+                      </FormControl>
+                    </PopoverTrigger>
+                  </Popover>
+
+                </FormItem>
+
+              )}
+             >
+
+             </FormField>
           </div>
         </form>
       </Form>
