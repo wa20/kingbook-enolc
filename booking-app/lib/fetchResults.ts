74d3a1b2bd13a466b76bca87c32d484f474b3689
wael <78425538+wa20@users.noreply.github.com>
2024-05-19 12:02:29 +0100

    fetch results page update to create post request

--- booking-app/lib/fetchResults.ts
+++ booking-app/lib/fetchResults.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import { SearchParams } from "@/app/search/page";
-import { Result } from "postcss";
+import { Result } from "@/typings";
 
 const fetchResults = async (searchParams: SearchParams) => {
   const username = process.env.OXYLABS_USERNAME;
@@ -128,27 +128,28 @@ const fetchResults = async (searchParams: SearchParams) => {
   });
 
 
-  const response = await fetch('https://realtime.oxylabs.io/v1/queries', 
-  {
+  const response = await fetch("https://realtime.oxylabs.io/v1/queries", {
     method: "POST",
     body: JSON.stringify(body),
     next: {
-        revalidate: 60 * 60, //cache for 1 hour
+      revalidate: 60 * 60, // cache for 1 hour
     },
     headers: {
-        "content-type": "application/json",
-        Authorization: "Basic " + Buffer.from(`${username}:${password}`).toString("base64")
+      "Content-Type": "application/json",
+      Authorization:
+        "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
     },
   })
-  .then((Response) => response.json())
-  .then((data) => {
-    if((data) =>{
-        if(data.results.length === 0) return;
-        const result: Result = data.results[0]
+  .then((response) => response.json())
+    .then((data) => {
+      if (data.results.length === 0) return;
+      const result: Result = data.results[0];
 
-        return result;
+      return result;
     })
-  })
+    .catch((err) => console.log(err));
+
+  return response;
 };
 
 export default fetchResults;
